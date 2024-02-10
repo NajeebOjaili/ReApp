dce967aa25cd7dd6710c7e38b0579403cb5893c9
NajeebOjaili <najeebojail@gmail.com>
2024-02-10 11:55:09 +0300

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const startBtn = document.getElementById("start-btn");
 const stopBtn = document.getElementById("stop-btn");
+const saveBtn = document.getElementById("save-btn");
 const recordedAudio = document.getElementById("recorded-audio");
 
 let mediaRecorder;
@@ -29,10 +30,26 @@ stopBtn.addEventListener("click", () => {
   mediaRecorder.stop();
   startBtn.disabled = false;
   stopBtn.disabled = true;
+  saveBtn.disabled = false;
 });
 
 mediaRecorder.addEventListener("stop", () => {
   const recordedBlob = new Blob(recordedBlobs, { type: "audio/mp3" });
   recordedAudio.src = URL.createObjectURL(recordedBlob);
   recordedAudio.play();
+});
+
+saveBtn.addEventListener("click", () => {
+  const blob = new Blob(recordedBlobs, { type: "audio/mp3" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.style.display = "none";
+  a.href = url;
+  a.download = "recording.mp3";
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
 });
